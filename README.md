4b2b7961aa2a8350c063e58ee5146db7ff97a02c
Robert Nac <robertnac@gmail.com>
2019-07-02 10:45:49 +0200

    New Produkt hinzu

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # spring_boot_mysql
 Tutorial von Spring Boot mit MySql
+
+Links: 
+https://www.youtube.com/watch?v=QloyS2dt9T4
+https://www.codejava.net/frameworks/spring-boot/spring-boot-crud-example-with-spring-mvc-spring-data-jpa-thymeleaf-hibernate-mysql
--- src/main/java/de/ronasoft/springboot/mysql/controller/AppController.java
+++ src/main/java/de/ronasoft/springboot/mysql/controller/AppController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import de.ronasoft.springboot.mysql.domain.Product;
 import de.ronasoft.springboot.mysql.service.ProductService;
@@ -22,5 +24,20 @@ public class AppController {
 	    model.addAttribute("listProducts", listProducts);
 	     
 	    return "index";
+	}
+	
+	@RequestMapping("/new")
+	public String showNewProductPage(Model model) {
+	    Product product = new Product();
+	    model.addAttribute("product", product);
+	     
+	    return "new_product";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveProduct(@ModelAttribute("product") Product product) {
+	    service.save(product);
+	     
+	    return "redirect:/";
 	}
 }
